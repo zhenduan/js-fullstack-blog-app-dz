38e3f5283000df80933ecf5ff1c149c446b4b833
zhenduan <duan2439@gmail.com>
2025-03-08 20:05:06 +1100

    create auth middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+
+function authMiddleware(req, res, next) {
+  const token = req.header("Authorization");
+  if (!token) return res.status(401).json({ error: "Access denied" });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    res.status(400).json({ error: "Invalid token" });
+  }
+}
+
+export default authMiddleware;
