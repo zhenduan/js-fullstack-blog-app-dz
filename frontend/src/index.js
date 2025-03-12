7a1c08ff587fffd90ba96110b139901ad90457a3
Zhen Duan <zhen@arkade.com.au>
2025-03-12 23:00:11 +1100

    implement protect route

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import BlogDetailPage from "./pages/BlogDetailPage";
 import CreateBlogPage from "./pages/CreateBlogPage";
@@ -12,7 +17,12 @@ import RegisterPage from "./pages/RegisterPage";
 import ProfilePage from "./pages/ProfilePage";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import useAuthStore from "./stores/authStore";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const PrivateRoute = ({ children }) => {
+  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+  return isAuthenticated ? children : <Navigate to="/login" />;
+};
 root.render(
   <React.StrictMode>
     <ToastContainer />
@@ -20,8 +30,24 @@ root.render(
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route exact path="/blogs/:id" element={<BlogDetailPage />} />
-        <Route exact path="/blog/create" element={<CreateBlogPage />} />
-        <Route exact path="/blog/create" element={<EditBlogPage />} />
+        <Route
+          exact
+          path="/blog/create"
+          element={
+            <PrivateRoute>
+              <CreateBlogPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          exact
+          path="/blog/edit/:edit"
+          element={
+            <PrivateRoute>
+              <EditBlogPage />
+            </PrivateRoute>
+          }
+        />
         <Route exact path="/login" element={<LoginPage />} />
         <Route exact path="/register" element={<RegisterPage />} />
         <Route exact path="/profile" element={<ProfilePage />} />
