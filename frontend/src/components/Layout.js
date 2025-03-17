909781764027f1c47120a8d120d602f9a3871e32
zhenduan <duan2439@gmail.com>
2025-03-17 16:41:23 +1100

    add notification

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -6,6 +6,11 @@ const Layout = ({ children }) => {
   return (
     <div>
       <Header />
+      <div className="text-center bg-gray-100 py-3  flex justify-center items-center">
+        <p>
+          Blogs may take a moment to load as the App is hosted on free services.
+        </p>
+      </div>
       <main className="flex-grow container mx-auto p-4 min-h-screen">
         {children}
       </main>
